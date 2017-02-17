8c9eb8ed28799701a657e883f422322bf1951795
shaikafreen <ShaikAfreen@users.noreply.github.com>
2017-02-17 20:55:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+
+var element = document.getElementById('main-text');
+
+element.innerHTML = 'New value';
+
+var img = document.getElementById('img');
+img.onclick = function () {
+   log.style.marginleft = '100px';   
+};
