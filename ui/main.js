d946d16c86ee0453547b8b562663babf31970bec
shaikafreen <ShaikAfreen@users.noreply.github.com>
2017-02-17 21:06:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'New value';
 
-var img = document.getElementById('img');
-img.onclick = function () {
+var img = document.getElementById('madi');
+madi.onclick = function () {
    log.style.marginleft = '100px';   
 };
