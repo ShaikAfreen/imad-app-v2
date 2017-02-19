86c1b3d6c0b0628f4f30644067604702306adf56
shaikafreen <ShaikAfreen@users.noreply.github.com>
2017-02-19 17:47:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/', function (req, res) {
  var names=[];
 app.get('/submit-name', function (req, res){
   //get name from request object
-  var name = req.params.name;
+  var name = req.query.name;
   names.push(name);
   res.send(JSON.stringify(names));
 });
