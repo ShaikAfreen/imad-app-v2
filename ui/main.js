de77adceaf60a804161a3ddad80c15c852113cba
shaikafreen <ShaikAfreen@users.noreply.github.com>
2017-02-19 16:51:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,21 @@
-console.log('Loaded!');
-
-var element = document.getElementById('main-text');
-
-element.innerHTML = "New value";
-
-var img = document.getElementById('madi');
-
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + "px";
+//counter code
+var button = document.getElementById('counter');
+button.onclick = function () {
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //take some action
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //not done  yet
+    };
+    //make the request
+    request.open('GET','http://shaikafreen.imad.hasura-app.io/counter',true);
+    request.send(null);
 }
-madi.onclick = function () {
-   var interval = setInterval(moveRight, 10);   
-};
