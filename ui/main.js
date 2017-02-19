4aa9b943d0ca043d77aff04eb2acc374bbac81cd
shaikafreen <ShaikAfreen@users.noreply.github.com>
2017-02-19 18:07:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,15 +20,32 @@ button.onclick = function () {
     request.send(null);
 };
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-    var name = ['name1','name2','name3','name4'];
-    var list = '';
-    for (var i=0;i<names.length;i++) {
-        list += '<li>'+names[i]+'</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //take some action
+            if (request.status === 200) {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list ='';
+                for (var i=0;i< names.length; i++) {
+                    list += '<li>'+names[i]+ '</li>';
+                }
+                var ui = document.getElementById('namelist');
+                ui.innerHTML = list;
+            }
+        }
+        //not done  yet
+    };
+    //make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET','http://shaikafreen.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
+    
 };
